e82bfbc2c4de32cfc6ccb3ed48d6a2c32ba7f04a
Militaru Adrian <adrian.militaru@avaelgo.ro>
2022-03-31 19:24:20 +0300

    Added new function for changing score

--- src/benchmarks/Algorithm.java
+++ src/benchmarks/Algorithm.java
@@ -7,6 +7,10 @@ class Algorithm {
         return computedScore+1;
     }
 
+    public int changeComputedScoreByNumber(int y){
+        return computedScore*y;
+    }
+
     public static void main(String args[]){
         System.out.println("CO!");
         System.out.println("CO! CO!");
