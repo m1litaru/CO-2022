04f0d428d78f752230f8411e6f6ac731faea02a1
Militaru Adrian <adrian.militaru@avaelgo.ro>
2022-03-31 18:05:46 +0300

    Improved main in Algorithm.java

--- src/benchmarks/Algorithm.java
+++ src/benchmarks/Algorithm.java
@@ -4,7 +4,8 @@ class Algorithm {
     float computedScore;
 
     public static void main(String args[]){
-        System.out.println("CO!")
+        System.out.println("CO!");
+        System.out.println("CO! CO!");
     }
 
 }
